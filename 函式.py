f0fafa9f17a594b8de9e8876c42f85fcf3b97dd2
farmertim <52063135+farmertim@users.noreply.github.com>
2020-01-18 14:08:07 +0800

    give function preset

--- 函式.py
+++ 函式.py
@@ -1,3 +1,4 @@
+apple=[0]
 def odd(number1,number2):
     return number1+number2  #直接寫return 不用像C要有回傳型態
     #print("Answer is:",number1+number2)
@@ -7,6 +8,20 @@ def multiplication(number1,number2):
     print("Answer is :",number1*number2)
 def division(number1,number2):
     print("Answer is :",number1/number2)
+def check(name,name1):
+    print(name)
+    print(name1)
+def private1(p=2):#可以預設數字
+    print(p)
+def more(*NAME):
+    print(NAME)
+    print("ONE:",NAME[6])
+    apple[0]=NAME
+
+more(5,9,10,5,6,8,9,9,9,9)
+check(name1=5,name=3) #對應相同的參數名稱 
+private1()
+print("apple:",apple)
 while True:
     n1=int(input("輸入數字1:"))
     n2=int(input("輸入數字2:"))
@@ -21,3 +36,4 @@ while True:
         division(n1,n2)
     else:
         print("False")
+
