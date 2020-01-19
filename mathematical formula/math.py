0521491ae39019ea71235aa9029eee0aab539023
farmertim <52063135+farmertim@users.noreply.github.com>
2020-01-19 22:34:36 +0800

    formula

---
+++ mathematical formula/math.py
@@ -0,0 +1,26 @@
+def Add(*number):#加法
+    total=0
+    for num in number:
+        total+=num
+    return total
+def Subtraction(*number):#減法
+    total=0
+    for num in number:
+        total-=num
+    return total
+def multiplication(*number):#乘法
+    total=1
+    for num in number:
+        total*=num
+    return total
+def division(*number):
+    total=number[0]
+    count=0
+    for num in number:
+        if count!=0:
+            total/=num
+            
+        count+=1
+    return total
+print(division(9,3,2))
+
