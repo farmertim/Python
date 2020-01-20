2475c1ab6e3eeb62bfd5961c94b8e772fd0699fc
farmertim <52063135+farmertim@users.noreply.github.com>
2020-01-20 10:44:00 +0800

    json 讀檔寫檔

---
+++ mathematical formula/json讀檔寫檔.py
@@ -0,0 +1,18 @@
+import json
+with open("practice.json",mode="r",encoding="utf-8")as file:
+    data=json.load(file)
+    print(data)#data是一個字典資料
+    name=data["name"]
+    grade=data["grade"]
+print(grade)
+number=int(input("輸入一個數字:"))
+sex=input("輸入性別:")
+name=input("輸入name")
+data["grade"]=number
+data["name"]=name
+data["sex"]=sex
+print(name)
+with open("practice.json",mode="w",encoding="utf-8")as file:
+    json.dump(data,file)
+
+    
