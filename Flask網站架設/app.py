96c6ac6612647df64dd1b599360485c40a1013d1
farmertim <52063135+farmertim@users.noreply.github.com>
2020-01-31 22:31:21 +0800

    網站架設

--- Flask網站架設/app.py
+++ Flask網站架設/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-app=Flask(__name__)#__name__代表目前執行的模組
-@app.route("/")#函式的裝飾 已函式為基礎 ，提供附加的功能
+app1=Flask(__name__)#__name__代表目前執行的模組
+@app1.route("/")#函式的裝飾 已函式為基礎 ，提供附加的功能
 def home():
     
-    return "Hello Flask22" 
-@app.route("/text")
+    return "Hello Flask223333" 
+@app1.route("/text")
 def text():
    
     return "this is text"
 if __name__=="__main__":#如果以主程式執行
-    app.run()#立刻啟動伺服器
+    app1.run()#立刻啟動伺服器
